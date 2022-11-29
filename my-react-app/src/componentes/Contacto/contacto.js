13b782708dde19ff8a51fbe3dc1c8e1a33dd34a1
Chipi87 <mgauto@outlook.es>
2022-11-29 20:55:20 -0300

    Final

--- my-react-app/src/componentes/Contacto/contacto.js
+++ my-react-app/src/componentes/Contacto/contacto.js
@@ -18,24 +18,26 @@ const Contacto = () => {
                     <form onSubmit={handleSubmit(onSubmit)} className="contact">
                         <h2> Formulario de Contacto </h2>
                         <div className="contactregister">
-                            <input type="text" placeholder="Nombre" {...register("nombre", { require: true, maslength: 10 })} className="typetext" />
-                            {errors.nombre?.type === "required" && <p> El campo nombre es obligatorio </p>}
-                            {errors.nombre?.type === "maxlength" && <p> El campo debe contener mas 10 caracteres </p>}
+                            <input type="text" placeholder="Nombre" {...register('nombre', { require: true, maslength: 10 })} className="typetext" />
+                            {errors.nombre?.type === 'required' && <p> El campo nombre es obligatorio </p>}
+                            {errors.nombre?.type === 'maxlength' && <p> El campo debe contener mas 10 caracteres </p>}
                         </div>
-                        <div className="contactregister">
 
-                            <input type="text" placeholder="Apellido"{...register("apellido", { require: true, maslength: 10 })} className="typetext" />
+                        <div className="contactregister">
+                            <input type="text" placeholder="Apellido"{...register('apellido', { require: true})} className="typetext" />
                         </div>
+
                         <div className="contactregister">
-                            <input type="tel" placeholder="Telefono"{...register("number", { require: true, minlength: 5 })} className="typetext" /></div>
+                            <input type="tel" placeholder="Telefono"{...register('number', { require: true})} className="typetext" /></div>
                         <div className="contactregister">
-                            <input type="number" min="1" max="8" placeholder="Personas" step="1"{...register("tel", { require: true, maslength: 10 })} className="typetext" />
+                            <input type="number" min="1" max="8" placeholder="Personas" step="1"{...register('tel', { require: true })} className="typetext" />
                             <div className="contactregister">
-                                <input type="email" placeholder="Email" {...register("email", { pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ })} className="typetext" />
-                                {errors.nombre?.type === "pattern" && <p> El formato es incorrecto </p>}
+                                <input type="email" placeholder="Email" {...register('email', { pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ })} className="typetext" />
+                                {errors.email?.type === 'pattern' && <p> El formato es incorrecto </p>}
                             </div>
+
                             <div className="contactregister">
-                                <textarea type="text" placeholder="Comentario" size="small" {...register("apellido", { require: true, minlength: 10 })} className="typetext">
+                                <textarea type="text" placeholder="Comentario" size="small" {...register("text", { require: true, minlength: 10 })} className="typetext">
                                 </textarea>
                             </div>
                             <label> Tipo de cabaña</label>
